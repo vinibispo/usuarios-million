e5a3573277c1bf0f5d07f5341509b09223e30609
vinibispo <vini.bispo015@gmail.com>
2020-04-02 15:34:57 -0300

    Adicionando isso para não mostrar warning

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const uri = 'mongodb://localhost:27017/millionUsuarios';
 
-mongoose.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose; 
